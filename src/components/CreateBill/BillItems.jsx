a2623e2c505c32711b2f75e5a876e56de5918c37
Rizan Mohomed <musthafarizan@gmail.com>
2018-11-26 16:30:29 +0530

    added sub total row

--- src/components/CreateBill/BillItems.jsx
+++ src/components/CreateBill/BillItems.jsx
@@ -17,6 +17,13 @@ const BillItems = props => {
         <BillItem />
         <BillItem />
       </tbody>
+      <tfoot>
+        <tr className="bg-secondary text-light font-weight-bold">
+          <td colSpan="2">Sub Total</td>
+          <td>6</td>
+          <td>6000</td>
+        </tr>
+      </tfoot>
     </table>
   );
 };
