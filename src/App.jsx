0c73e50891f0deb97894852c6c7f7e7cbab46692
Rizan Mohomed <musthafarizan@gmail.com>
2018-11-26 15:14:13 +0530

    initial markup setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="w3-container">
+        <div className="w3-row">
+          <div className="w3-col s6">
+            <h3>Create Bill</h3>
+          </div>
+
+          <div className="w3-col s6">
+            <h3>Bills</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
